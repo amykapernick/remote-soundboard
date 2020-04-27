6b595666f38e13bdc115fc39193e32c03f013673
Amy Kapernick <amy@aimhigherweb.design>
2020-04-27 12:13:38 +0800

    have sync and button function working

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+
+const express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser'),
+	Twilio = require('twilio'),
+syncSid = process.env.TWILIO_SYNC_SERVICE_SID
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {	
+	res.sendFile('./index.html', { root: __dirname })
+})
+
+app.post('/sync', (req, res) => {
+	const client = new Twilio(
+        process.env.TWILIO_API_KEY,
+        process.env.TWILIO_API_SECRET,
+        {accountSid: process.env.TWILIO_ACCOUNT_SID}
+	),
+	service = client.sync.services(syncSid),
+	sound = req.body.sound
+
+	service.documents('sound').update({
+		data: {
+			sound
+		}
+	})
+
+	res.end()
+})
+
+
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`Example app listening on port ${process.env.PORT || 3000}!`)
+})
